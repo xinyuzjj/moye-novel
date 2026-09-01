6ea8e99188c9101583eb66b2a578032697646690
xinyuzjj <xinyuzjj@users.noreply.github.com>
2026-09-01 21:43:08 +0800

    chore: bump version to 2.7.1

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@
 (function () {
   'use strict';
 
-  const APP_VERSION = '2.7.0';
+  const APP_VERSION = '2.7.1';
 
   const $ = (s, r) => (r || document).querySelector(s);
   const $$ = (s, r) => Array.prototype.slice.call((r || document).querySelectorAll(s));
